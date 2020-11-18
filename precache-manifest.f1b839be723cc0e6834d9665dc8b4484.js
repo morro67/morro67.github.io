27cb22415c3ae21f9825733438184cc8b8339b90
morro67 <50253355+morro67@users.noreply.github.com>
2020-11-18 19:12:19 +0300

    Add files via upload

---
+++ precache-manifest.f1b839be723cc0e6834d9665dc8b4484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc270ee405ede5082508fda9f93151d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f26d050bf104adde425d",
+    "url": "/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "f26d050bf104adde425d",
+    "url": "/static/js/2.dba712ba.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dba712ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038afc826cb0e1b09f4d",
+    "url": "/static/js/main.1299c610.chunk.js"
+  },
+  {
+    "revision": "b8ec7d1d142bf18f09cd",
+    "url": "/static/js/runtime-main.e8dece5c.js"
+  }
+]);
