e92c1fd7df515881617fd5840fc249029841326d
morro67 <50253355+morro67@users.noreply.github.com>
2020-11-18 18:11:36 +0300

    Add files via upload

---
+++ precache-manifest.8466effab2c2f4684a4e78d36752a3dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685c8a858a4b01afc049ea4db21393ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41e88908298338afd54b",
+    "url": "/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "d64bbf2984244351f7e5",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "41e88908298338afd54b",
+    "url": "/static/js/2.2aff2571.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2aff2571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64bbf2984244351f7e5",
+    "url": "/static/js/main.e5cc8c93.chunk.js"
+  },
+  {
+    "revision": "b8ec7d1d142bf18f09cd",
+    "url": "/static/js/runtime-main.e8dece5c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
